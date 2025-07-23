d36f9bbaefa9540da62eb7a9dd2e09c941e6bf11
Nguyen Vo Anh Tu <nguyenvoanhtu9@gmail.com>
2025-07-23 13:43:15 +0700

    Add BLE_Class and change to config WIFI via BLE

--- PP4.h
+++ PP4.h
@@ -1,9 +1,9 @@
 #ifndef _PP4_H_
 #define _PP4_H_
 #include <Arduino.h>
-#define DEBUG
+// #define DEBUG
 
-#define VERSION "v16.7.0"
+#define VERSION "v23.7.0"
 #define MQTT_TOPIC_MAX_LEN 80
 
 // Định nghĩa hằng số
